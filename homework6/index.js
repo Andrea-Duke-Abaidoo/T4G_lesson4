51c5d6ca15569ba52e5a574917dd123858d1e10f
Andrea-Duke-Abaidoo <andreadukeabaidoo@gmail.com>
2024-12-09 13:03:07 +0000

    first commit

---
+++ homework6/index.js
@@ -0,0 +1,20 @@
+let name = "Andrea Duke Abaidoo";
+console.log(name);
+let age = 18;
+console.log(age);
+let sum =3 + 6;
+console.log(sum);
+let sub = 9-3;
+console.log(sub);
+let div = 9*2;
+console.log(div);
+let mut = 8 ^ 2;
+console.log(mut);
+let rem = 15%4;
+console.log(rem)
+let big = true;
+console.log(big);
+let meg =21;
+console.log(typeof(meg))
+let text = `string`;
+console.log(typeof(text))
---
+++ lesson4/src/index.js
@@ -0,0 +1,23 @@
+let num3 =10;
+let num4 =15;
+let result = num3+num4;
+console.log(result);
+alert(result);
+
+function addTwoNumbers(){
+    addition = no3+no4;
+    console.log(addition);
+}
+addTwoNumbers(123, 345);
+
+function sayHello(){
+    alert = prompt
+}
+
+function coolRice(salt, water, rice){
+alert(`I cooked ${rice} cups of rice with ${salt} a pinch of salt`)
+}
+cookRice( half, 300mls,20)
+
+function
+let A prompt(`How many burgers would you like`);

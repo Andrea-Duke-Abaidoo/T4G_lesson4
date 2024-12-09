77c42e1e338e9ded68e9bc3594fa4dca1895a31c
Andrea-Duke-Abaidoo <andreadukeabaidoo@gmail.com>
2024-12-09 13:05:06 +0000

    javascript

--- lesson4/src/index.js
+++ lesson4/src/index.js
@@ -1,23 +1,46 @@
-let num3 =10;
-let num4 =15;
-let result = num3+num4;
-console.log(result);
-alert(result);
+alert("hello world");
+alert("Andrea Duke Abaidoo");
 
-function addTwoNumbers(){
-    addition = no3+no4;
-    console.log(addition);
+// let name; declaring a variable called name 
+let name;
+prompt("What is your name");
+
+// declaring a variable called name
+let sardinbox = prompt("How many sardines do you want to buy");
+alert(sardin+"it is");
+alert("2"+"2");
+
+// variable naming convention
+num12
+NumFive
+let friend = prompt("What's your friend's name?");
+let age = prompt("What's your friend's age?" );
+let favColor = prompt("What is your friend's favColor?");
+
+alert("My beautiful friend's name is"+friend+". She is "+ age+ "years old and her favourite color is"+ favColor);
+
+
+
+
+let num1 = 3;
+let num2 = 5;
+let result = num1 +num2;
+console.log(result)
+let num3 = 10;
+let num4 = 15;
+let result2 = num3 +num4;
+console.log(result2)
+
+// function cookRice (salt, water, rice)
+ 
+
+function checkAge(){
+    let age = prompt ("what is your age?");
+    if (age<6){
+        alert(`you are ${age} years old and too young`)
+    }else if (age && 17>=age >=6){
+        alert(`You are ${age} years old and you have limited options   `)
+    }else if (age && age >=18){
+        alert (` You are ${age} years old you can acess all options`)
+    }
 }
-addTwoNumbers(123, 345);
-
-function sayHello(){
-    alert = prompt
-}
-
-function coolRice(salt, water, rice){
-alert(`I cooked ${rice} cups of rice with ${salt} a pinch of salt`)
-}
-cookRice( half, 300mls,20)
-
-function
-let A prompt(`How many burgers would you like`);

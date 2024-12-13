4cca174583cef6076b6cc3d265e25d6cc6c8d142
Andrea-Duke-Abaidoo <andreadukeabaidoo@gmail.com>
2024-12-13 13:06:29 +0000

    corrections

--- lesson4/src/index.js
+++ lesson4/src/index.js
@@ -1,46 +1,20 @@
-alert("hello world");
-alert("Andrea Duke Abaidoo");
-
-// let name; declaring a variable called name 
-let name;
-prompt("What is your name");
-
-// declaring a variable called name
-let sardinbox = prompt("How many sardines do you want to buy");
-alert(sardin+"it is");
-alert("2"+"2");
-
-// variable naming convention
-num12
-NumFive
-let friend = prompt("What's your friend's name?");
-let age = prompt("What's your friend's age?" );
-let favColor = prompt("What is your friend's favColor?");
-
-alert("My beautiful friend's name is"+friend+". She is "+ age+ "years old and her favourite color is"+ favColor);
-
-
-
-
-let num1 = 3;
-let num2 = 5;
-let result = num1 +num2;
-console.log(result)
-let num3 = 10;
-let num4 = 15;
-let result2 = num3 +num4;
-console.log(result2)
-
-// function cookRice (salt, water, rice)
- 
-
-function checkAge(){
-    let age = prompt ("what is your age?");
+function checkAge() {
+    let age= prompt("What is your age?");
     if (age<6){
-        alert(`you are ${age} years old and too young`)
-    }else if (age && 17>=age >=6){
-        alert(`You are ${age} years old and you have limited options   `)
-    }else if (age && age >=18){
-        alert (` You are ${age} years old you can acess all options`)
+        alert(`You are ${age} years old and you are to young.`);
+    
+    } else if(age<=6 && age<=7){
+    
+    alert(`You are ${age} years old and you have limited options.`);
+    
+    } else if(age >=18) {
+        alert(`You are ${age} years old and you can enjoy all the options.`);
+    } else {
+        alert(`Enter an age please.`);
     }
-}
+    }
+    
+    checkAge();
+    prompt();
+    
+    
